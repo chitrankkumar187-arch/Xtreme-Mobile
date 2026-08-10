964b960a14bb3957be44833e8cf0156c0474ab64
Chitrank Yadav <chitrankkumar187@gmail.com>
2026-08-10 08:50:50 +0530

    Add CPlayerTextDrawPool class for player text draws

---
+++ app/src/main/cpp/samp/net/playertextdrawpool.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <stdint.h>
+#include "../game/textdraw.h"
+
+#define MAX_PLAYER_TEXT_DRAWS 256
+
+class CPlayerTextDrawPool
+{
+public:
+    CPlayerTextDrawPool();
+    ~CPlayerTextDrawPool();
+
+    void New(uint16_t wTextDrawID, TEXT_DRAW_TRANSMIT* pTextDrawTransmit, const char* szText);
+    void Delete(uint16_t wTextDrawID);
+    void Draw();
+    void SetSelectState(bool bState, uint32_t dwColor = 0);
+    void SetText(uint16_t wTextDrawID, const char* szText);
+
+    CTextDraw* GetAt(uint16_t wTextDrawID)
+    {
+        if (wTextDrawID >= MAX_PLAYER_TEXT_DRAWS) return nullptr;
+        if (!m_bSlotState[wTextDrawID]) return nullptr;
+        return m_pTextDraw[wTextDrawID];
+    }
+
+private:
+    void SendClick();
+
+private:
+    uint8_t m_bSlotState[MAX_PLAYER_TEXT_DRAWS];
+    CTextDraw* m_pTextDraw[MAX_PLAYER_TEXT_DRAWS];
+
+    bool m_bSelectState;
+    uint32_t m_dwHoverColor;
+    uint16_t m_wClickedTextDrawID;
+};
